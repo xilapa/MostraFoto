b1198e2a747603ec4d0b0ffd0b3fa6532438f35f
dirceu junior <dirceu.sjr@gmail.com>
2021-06-07 20:49:35 -0300

    criação do sign-in component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { NotfoundComponent } from './errors/notfound/notfound.component';
+import { SignInComponent } from './home/sig-in/sign-in.component';
 import { PhotoListComponent } from './photos/photo-list/photo-list.component';
 import { PhotoListResolver } from './photos/photo-list/photo-list.resolver';
 import { PhotosFormComponent } from './photos/photos-form/photos-form.component';
 
 const routes: Routes = [
-  { path: 'user/:userName', component: PhotoListComponent, resolve: { photos: PhotoListResolver }},
-  { path: 'p/add', component: PhotosFormComponent},
-  { path: '**', component: NotfoundComponent}
+  { path: '', component: SignInComponent },
+  { path: 'user/:userName', component: PhotoListComponent, resolve: { photos: PhotoListResolver } },
+  { path: 'p/add', component: PhotosFormComponent },
+  { path: '**', component: NotfoundComponent }
 ];
 
 @NgModule({
---
+++ src/app/home/home.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SignInComponent } from './sig-in/sign-in.component';
+
+
+
+@NgModule({
+  declarations: [SignInComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class HomeModule { }
