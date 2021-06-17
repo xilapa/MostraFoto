91b6d895f6b21b8bfa5aa1afeb6903b2e2c4a30d
dirceu junior <dirceu.sjr@gmail.com>
2021-06-16 21:22:19 -0300

    validador assíncrono inserido no form de cadastro

--- src/app/home/sign-up/sign-up.component.ts
+++ src/app/home/sign-up/sign-up.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { lowerCaseValidator } from 'src/app/shared/validators/lowerCase.validator';
+import { UserNameTakenValidatorService } from './user-name-taken.validator.service';
 
 @Component({
   selector: 'app-sign-up',
@@ -11,12 +12,12 @@ export class SignUpComponent implements OnInit {
 
   signUpForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder, private userNameTakenService: UserNameTakenValidatorService) { }
   ngOnInit(): void {
     this.signUpForm = this.formBuilder.group({
       email: ['', [Validators.required, Validators.email]],
       fullName: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(40)]],
-      userName: ['', [Validators.required, lowerCaseValidator, Validators.minLength(2), Validators.maxLength(30)]],
+      userName: ['', [Validators.required, lowerCaseValidator, Validators.minLength(2), Validators.maxLength(30)], this.userNameTakenService.Validator()],
       password: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(14)]]
     })
   }
---
+++ src/app/home/sign-up/user-name-taken.validator.service.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { AbstractControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { debounceTime, first, map, switchMap } from 'rxjs/operators';
+
+const API_URL = "http://localhost:3000";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserNameTakenValidatorService {
+
+  constructor(private http: HttpClient) { }
+  
+  Validator() {
+    const checkUserNameTaken = (userName: string) : Observable<any> => 
+       this.http.get(API_URL + '/user/exists/' + userName)
+    
+    return (control: AbstractControl)=>{
+      return control.valueChanges
+        .pipe(debounceTime(300))
+        .pipe(switchMap(input => checkUserNameTaken(input)))
+        .pipe(map(isTaken => isTaken ? { userNameTaken: true } : null))
+        .pipe(first())
+    }
+  }
+}
