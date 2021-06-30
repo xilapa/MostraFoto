6549b0f011bf7d7ff80b168c7bd6a43080b40c66
dirceu junior <dirceu.sjr@gmail.com>
2021-06-30 18:54:35 -0300

    auth guard adicionado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,17 +2,19 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 
+
 import { NotfoundComponent } from './errors/notfound/notfound.component';
 import { PhotoListComponent } from './photos/photo-list/photo-list.component';
 import { PhotoListResolver } from './photos/photo-list/photo-list.resolver';
 import { PhotosFormComponent } from './photos/photos-form/photos-form.component';
 import { MeuTesteComponent } from './tooltipCSS/tooltipCSS.component';
+import { AuthGuard } from './core/guards/auth.guard';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home' },
   { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule)},
   { path: 'user/:userName', component: PhotoListComponent, resolve: { photos: PhotoListResolver } },
-  { path: 'p/add', component: PhotosFormComponent },
+  { path: 'p/add', component: PhotosFormComponent, canActivate:[AuthGuard] },
   { path: 'test', component: MeuTesteComponent},
   { path: '**', component: NotfoundComponent }
 ];
