84c29572c67d9aa9c769e1035cc599cf91df1432
dirceu junior <dirceu.sjr@gmail.com>
2021-07-09 12:46:36 -0300

    adição de observable nos parâmetros de rota para photoList

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 
 
@@ -11,7 +12,7 @@ import { PhotoService } from '../photo/photo.service';
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.css']
 })
-export class PhotoListComponent implements OnInit {
+export class PhotoListComponent implements OnInit, OnDestroy {
   
   public title = 'MostraFoto';
   public photos: IPhoto[] =[];
@@ -22,16 +23,23 @@ export class PhotoListComponent implements OnInit {
   public userName: string = '';
   public actualPage: number = 1;
 
+  public subscriptions = new Subscription();
+
+
   constructor(private activatedRoute: ActivatedRoute, private photoService: PhotoService) { }
   
   ngOnInit(): void {
-    this.userName = this.activatedRoute.snapshot.params.userName;
-    this.photos = this.activatedRoute.snapshot.data.photos;
-
+    let routeSubs = this.activatedRoute.paramMap.subscribe(params => {
+      this.userName = params.get('userName');
+      // toda vez que a rota mudar ele vai no resolver buscar os dados
+      this.photos = this.activatedRoute.snapshot.data.photos;
+    })
+    this.subscriptions.add(routeSubs);
   }  
   
-
-  
+  ngOnDestroy(): void {
+    this.subscriptions && this.subscriptions.unsubscribe()
+  }
 
   onClick() : void{
     this.photoService.listFromUserPaginated(this.userName, ++this.actualPage)
