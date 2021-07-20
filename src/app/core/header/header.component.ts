5c47454268912d06431fa2b51e5f35341e3bb799
dirceu junior <dirceu.sjr@gmail.com>
2021-07-20 13:01:30 -0300

    menu componente feito + clique com enter

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
-import { Observable } from 'rxjs';
+import { fromEvent, Observable, Subscription } from 'rxjs';
+import { filter, map, tap } from 'rxjs/operators';
+import { MenuComponent } from 'src/app/shared/components/menu/menu.component';
 import { IUser } from '../user/IUser';
 import { UserService } from '../user/user.service';
 
@@ -9,16 +11,36 @@ import { UserService } from '../user/user.service';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, AfterViewInit {
 
   user$: Observable<IUser>;
+  keyDownSubs: Subscription;
 
-  constructor( private userService: UserService, private router: Router) {
+  @ViewChild('menu') menuComponent: MenuComponent;
+
+
+  constructor(private userService: UserService, private router: Router) {
     this.user$ = userService.getUser();
   }
-  
+  ngOnInit(): void {
+
+  }
+
+  ngAfterViewInit(): void {
+    this.keyDownSubs = fromEvent(window, 'keydown')
+      .pipe(tap(e => {
+        if ((e as KeyboardEvent).key != "Enter") e.preventDefault()
+      }))
+      .pipe(filter(e => (e as KeyboardEvent).key == "Enter" && !(e.target as HTMLElement).classList.contains('rounded')))
+      .subscribe(() =>
+        this.menuComponent.toggle());
+
+  }
+
   logout(): void {
     this.userService.logout();
     this.router.navigate([''])
   }
+
+
 }
